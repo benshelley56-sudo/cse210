d49b3aaf5137de51ee636f4166f0cae05aa17c67
Ben Shelley <benshelley56@gmail.com>
2025-10-15 00:03:34 -0600

    First Private classes assignment

---
+++ prepare/Learning03/privateclass_test.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+    public Fraction(int wholenumber)
+    {
+        _top = wholenumber;
+        _bottom = 1;
+
+    }
+    public Fraction(int denom, int num)
+    {
+        _top = num;
+        _bottom = denom;
+
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+        Console.WriteLine(top);
+    }
+
+    public int GettBottom()
+    {
+        return _bottom;
+    }
+    
+        public void SetBottom(int bottom)
+    {
+        _top = bottom;
+        Console.WriteLine();
+    }
+
+    public string GetFractionString()
+    {
+
+        string text = $"{_top}/{_bottom}";
+
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top /  (double)_bottom;
+ 
+    }
+
+}
